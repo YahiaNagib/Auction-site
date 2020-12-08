4fec84f1b3067385aa6b8f0466e596f6bbbba14c
YahiaNagib <yahianagib123@gmail.com>
2020-12-08 19:53:32 +0200

    create models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,44 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    start_bid = models.FloatField()
+    created_date = models.DateTimeField(default=timezone.now)
+    image_URL = models.URLField(blank=True, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Bid(models.Model):
+    bid = models.FloatField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Listing: {listing.title}, value:{self.bid}"
